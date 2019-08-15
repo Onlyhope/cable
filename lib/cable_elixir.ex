3466099e08b6014f284afce7033c931569ff5ded
Aaron Lee <aaron.lee2@mastercard.com>
2019-08-15 11:04:36 -0400

    Initial commit of boilerplate

---
+++ lib/cable_elixir.ex
@@ -0,0 +1,18 @@
+defmodule CableElixir do
+  @moduledoc """
+  Documentation for CableElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CableElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cable_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule CableElixirTest do
+  use ExUnit.Case
+  doctest CableElixir
+
+  test "greets the world" do
+    assert CableElixir.hello() == :world
+  end
+end
